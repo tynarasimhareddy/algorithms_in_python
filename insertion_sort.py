d501b714dbe12a181d518f80bef89f111eaac48b
tynarasimhareddy <tynarasimhareddy@gmail.com>
2019-09-14 23:36:54 +0530

    updated with tested code

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,15 +1,16 @@
 
 # python 3.x
 def insertion_sort(arr):
-  for i in range(len(arr)):
-    v = arr[i]
-    j = i
-    while j >=1 and v < arr[j-1]:
-      arr[j] = arr[j-1]
-      j -= 1
-    arr[j] = v
+    for i in range(len(arr)):
+        v = arr[i]
+        j = i
+        while(arr[j-1] > v and j>=1):
+            arr[j] = arr[j-1]
+            j -= 1
+        arr[j] = v
 
-arr = [4,5,10,2,6,11]
+
+input = [4,5,10,2,6,11]
 print('Input array = {}'.format(input))
 insertion_sort(input)
 print('Sorted result = {}'.format(input))

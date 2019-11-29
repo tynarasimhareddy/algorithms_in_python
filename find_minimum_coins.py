f657635034396871162d6c855b847a52e8e3b35c
tynarasimhareddy <tynarasimhareddy@gmail.com>
2019-11-29 15:45:36 +0530

    optimized by removing additional argument
    avoiding the length of coins passing as argument to minCoins method and removed additional range() call.

--- find_minimum_coins.py
+++ find_minimum_coins.py
@@ -2,21 +2,20 @@
 import sys 
 
 # m is size of coins array (number of different coins) 
-def minCoins(coins, m, V):
+def minCoins(coins, V):
     table = {}
     table[0] = 0
     for i in range(1, V+1):
         table[i] = sys.maxsize
 
     for i in range(1, V+1):
-        for j in range(m):
-            if coins[j] <= i:
-                sub_res = table[i - coins[j]]
+        for c in coins:
+            if c <= i:
+                sub_res = table[i - c]
                 if sub_res != sys.maxsize and sub_res + 1 < table[i]:
                     table[i] = sub_res + 1
     return table[V]
 
 coins = [9, 6, 5, 1] 
-m = len(coins) 
 V = 8
-print("Minimum coins required is",minCoins(coins, m, V)) 
+print("Minimum coins required is",minCoins(coins, V)) 

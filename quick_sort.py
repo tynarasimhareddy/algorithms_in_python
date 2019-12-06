71b8f6b89a1c6c3053a1b515c32d04cf86d99504
tynarasimhareddy <tynarasimhareddy@gmail.com>
2019-12-06 14:05:33 +0530

    Corrected the logic

--- quick_sort.py
+++ quick_sort.py
@@ -1,28 +1,25 @@
 def quick_sort(arr, low, high):
-    if low < high:
-        pivot = partition(arr, low, high)
-        quick_sort(arr, low, pivot)
-        quick_sort(arr, pivot+1, high)
+        if low < high:
+                pivot = find_pivot(arr, low, high)
+                quick_sort(arr, low, pivot - 1)
+                quick_sort(arr, pivot+1, high)
 
-def partition(arr, low, high):
-    pivot = arr[low]
-    left = low
-    right = high
-    while left < right:
-        while arr[left] <= pivot:
-            left += 1
-        while arr[right] > pivot:
-            right -= 1
-        if left < right:
-            arr[left], arr[right] = arr[right], arr[left]
-    arr[low], arr[right] = arr[right], pivot
+def find_pivot(arr, start, end):
+        pivot = arr[start]
+        left = start + 1
+        right = end
+        while left <= right:
+                while left <= right and arr[left] <= pivot:
+                        left += 1
+                while left <= right and arr[right] > pivot:
+                        right -= 1
+                if left < right:
+                        arr[left], arr[right] = arr[right], arr[left]
+        arr[start], arr[right] = arr[right], pivot
+        return right
 
-    return right
-
-inp = [64, 34, 25, 12, 22, 11, 90] 
-
-print('Input array = {}'.format(inp))
-
-quick_sort(inp, 0, len(inp) - 1)
-
-print('Sorted array = {}'.format(inp))
+arr = [4, 7, 9, 3, 2, 6, 1]
+arr = [64, 34, 25, 12, 22, 11, 90]
+print('Input = {}'.format(arr))
+quick_sort(arr, 0, len(arr) - 1)
+print('Sorted output = {}'.format(arr))
